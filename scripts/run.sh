4fc767f114bcc4683391eef386d66b9d62e7b2db
ayrapetovai <ayeaye.post@gmal.com>
2015-12-14 00:17:21 +0300

    minor fixes

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-java -cp ../roguelike.jar;../libs/* -Djava.library.path=../natives ru.ayeaye.game.main.MainClass
+java -cp ../roguelike.jar:../libs/* -Djava.library.path=../natives ru.ayeaye.game.main.MainClass
 
